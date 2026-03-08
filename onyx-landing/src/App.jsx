31d9101702a5232004e30eb157672dadc38854a3
Clookys <82194138+Clookys@users.noreply.github.com>
2026-03-08 20:22:59 +0100

    Delete onyx-landing directory

--- onyx-landing/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import Landing from "./Landing.jsx";
-import Radar from "./Radar.jsx";
-
-export default function App() {
-  const [page, setPage] = useState("landing");
-  return page === "radar" ? <Radar onBack={() => setPage("landing")} /> : <Landing onOpenRadar={() => setPage("radar")} />;
-}
